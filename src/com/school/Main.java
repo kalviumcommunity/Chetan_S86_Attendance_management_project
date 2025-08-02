825e8d108bd86689c605aaa5059462d504ace86d
Chetan Rayalu <$ chetanrayaluthirumalasetty@gmail.com>
2025-08-02 10:12:44 +0530

    Done

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,6 +2,25 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Attendance Management Project");
+        Student[] students = {
+            new Student("Lokesh", 101),
+            new Student("Bhavdeep", 102),
+            new Student("Eswar", 103)
+        };
+
+        Course[] courses = {
+            new Course("DBMS", "MTH101"),
+            new Course("OOPS", "PHY101")
+        };
+
+        System.out.println("Student Details:");
+        for (Student s : students) {
+            s.display();
+        }
+
+        System.out.println("\nCourse Details:");
+        for (Course c : courses) {
+            c.display();
+        }
     }
 }
